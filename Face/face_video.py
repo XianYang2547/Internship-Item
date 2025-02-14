66a8829b5096db676a70ee7689e3eb1207a83b42
xianyang <xy_mts@163.com>
2025-02-14 18:03:17 +0800

    push all...

---
+++ Face/face_video.py
@@ -0,0 +1,245 @@
+import random
+import threading
+from collections import Counter
+from copy import deepcopy
+
+import cv2
+import numpy as np
+import torch
+
+from my_train import device, My_Face
+from utils import common
+from utils.DBFace import DBFace
+from utils.common import is_point_nearby, crop_face, apply_gaussian_blur
+from utils.db_pro import Face_comparison
+from utils.db_pro import add_filedoer
+from utils.db_pro import detect_images, detect
+
+# %%
+thread_lock = threading.Lock()
+thread_exit = False
+# --------加载DBface检测模型--------#
+dbface = DBFace()
+dbface.eval()
+dbface.cuda()
+dbface.load('weight/dbface.pth')
+# --------加载识别模型--------#
+net = My_Face().to(device)
+net.load_state_dict(torch.load('weight/last.pt'))
+net.eval()
+
+
+# %%
+class myThread(threading.Thread):
+    def __init__(self, camera_id, img_height, img_width):
+        super(myThread, self).__init__()
+        self.camera_id = camera_id
+        self.img_height = img_height
+        self.img_width = img_width
+        self.frame = np.zeros((img_height, img_width, 3), dtype=np.uint8)
+        self.frames = np.zeros((img_height, img_width, 3), dtype=np.uint8)
+
+    def get_frame(self):
+        return self.frame, self.frames
+
+    def run(self):
+        global thread_exit
+        cap = cv2.VideoCapture(self.camera_id)
+
+        radius = 100
+        thickness = 1
+        while not thread_exit:
+            ret, frame = cap.read()
+            self.frames = deepcopy(frame)
+            if ret:
+                frame = cv2.flip(frame, 1)
+                frame = cv2.resize(frame, (self.img_width, self.img_height))
+                thread_lock.acquire()
+                height, width, _ = frame.shape  # 获取视频帧的中心点坐标
+                center_x = int(width / 2)
+                center_y = int(height / 2)
+                color0 = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+                cv2.circle(frame, (center_x, center_y), radius, color0, thickness)
+                cv2.line(frame, (center_x - 10, center_y), (center_x + 10, center_y), (0, 255, 0), thickness)
+                cv2.line(frame, (center_x, center_y - 10), (center_x, center_y + 10), (0, 255, 0), thickness)
+                frame = apply_gaussian_blur(frame, center_x, center_y, radius)  # 特写，圆外模糊
+                self.frame = frame
+                thread_lock.release()
+            else:
+                thread_exit = True
+        cap.release()
+        cv2.destroyAllWindows()
+
+
+def Face_Entry_Pro():
+    global thread_exit
+    camera_id = 1
+    img_height = 480
+    img_width = 640
+    # 调用摄像头线程
+    thread = myThread(camera_id, img_height, img_width)
+    thread.start()
+
+    name = input('input your name ...\n')
+    count = 0
+    interval = 10
+    front, up, bow, left, right = 0, 0, 0, 0, 0
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    thickness = 1
+    font_scale = 0.5  # 字体大小
+    face_list = []
+    while not thread_exit:
+        thread_lock.acquire()
+        frame, frames = thread.get_frame()
+        if count % interval == 0:
+            height, width, _ = frame.shape  # 获取视频帧的中心点坐标
+            center_x = int(width / 2)
+            center_y = int(height / 2)
+            position = (center_x - 80, center_y - 100)  # 文本位置
+
+            objs = detect(dbface, frames)
+            if len(objs) == 1:  # 一次只录入一个人
+                if is_point_nearby((center_x, center_y), objs[0].landmark[2]):  # 判断鼻子关键点是否在圆内
+                    if 0 <= abs(objs[0].landmark[2][0] - center_x) <= 10 and 0 <= abs(
+                            objs[0].landmark[2][1] - center_y) <= 10:
+                        text0 = 'Full face Detected'
+                        color = (255, 99, 71)  # 字体颜色 (BGR格式)
+                        if front < 25:
+                            fronts = crop_face(frames, common.intv(objs[0].box), name, 'front', front)
+                            face_list.append(fronts)
+                            front += 1
+                        cv2.putText(frame, text0, position, font, font_scale, color, thickness, cv2.LINE_AA)
+
+                    elif center_y - objs[0].landmark[2][1] > 25:
+                        text1 = 'Head Up Detected'
+                        color = (75, 0, 130)  # 字体颜色 (BGR格式)
+                        if up < 25:
+                            ups = crop_face(frames, common.intv(objs[0].box), name, 'up', up)
+                            face_list.append(ups)
+                            up += 1
+                        cv2.putText(frame, text1, position, font, font_scale, color, thickness, cv2.LINE_AA)
+
+                    elif objs[0].landmark[2][1] - center_y > 30:
+                        text2 = 'Head bow Detected'
+                        color = (127, 255, 0)  # 字体颜色 (BGR格式)
+                        if bow < 25:
+                            bows = crop_face(frames, common.intv(objs[0].box), name, 'bow', bow)
+                            face_list.append(bows)
+                            bow += 1
+                        cv2.putText(frame, text2, position, font, font_scale, color, thickness, cv2.LINE_AA)
+
+                    elif center_x - objs[0].landmark[2][0] > 30:
+                        text3 = 'Turn left detected'
+                        color = (255, 127, 80)  # 字体颜色 (BGR格式)
+                        if left < 25:
+                            lefts = crop_face(frames, common.intv(objs[0].box), name, 'left', left)
+                            face_list.append(lefts)
+                            left += 1
+                        cv2.putText(frame, text3, position, font, font_scale, color, thickness, cv2.LINE_AA)
+
+                    elif objs[0].landmark[2][0] - center_x > 30:
+                        text4 = 'Turn right detected'
+                        color = (255, 0, 0)  # 字体颜色 (BGR格式)
+                        if right < 25:
+                            rights = crop_face(frames, common.intv(objs[0].box), name, 'right', right)
+                            face_list.append(rights)
+                            right += 1
+                        cv2.putText(frame, text4, position, font, font_scale, color, thickness, cv2.LINE_AA)
+
+                else:
+                    text = 'Please place your face in the circle'
+                    font = cv2.FONT_HERSHEY_SIMPLEX
+                    position = (center_x - 150, center_y - 100)  # 文本位置
+                    font_scale = 0.5  # 字体大小
+                    color = (0, 255, 0)  # 字体颜色 (BGR格式)
+                    thickness = 1  # 字体粗细
+                    cv2.putText(frame, text, position, font, font_scale, color, thickness, cv2.LINE_AA)
+                # ----------显示录入进度----------#
+                textt = (f"notice here\n"
+                         f"Full face: current:{front},total:25\n"
+                         f"Head Up: current:{up},total:25\n"
+                         f"Head bow: current:{bow},total:25\n"
+                         f"Turn left: current:{left},total:25\n"
+                         f"Turn righ: current:{right},total:25")
+                lines = textt.split('\n')
+                y = 20
+                for line in lines:
+                    cv2.putText(frame, line, (400, y), font, font_scale, (0, 255, 255), thickness, cv2.LINE_AA)
+                    y += 20
+                # -----------------------------#
+                cv2.imshow('Demo', frame)
+        count += 1
+        if front + up + bow + left + right == 125:
+            print(f"采集到{front + up + bow + left + right}张人脸"
+                  + '\n' + '即将录入人脸...' + '\n' + '注册完成...')
+            add_filedoer(name, face_list, net, device, path='weight/feature.pt')
+            print(front + up + bow + left + right)  # 100
+            cv2.destroyAllWindows()
+            thread_exit = True
+        thread_lock.release()
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            thread_exit = True
+    thread.join()
+
+
+def camera_demo0():
+    # --------加载人脸数据库--------#
+    face_data = torch.load('weight/feature.pt')
+    print('Running...')
+    # --------摄像头检测--------#
+    cap = cv2.VideoCapture(1)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+    ok, frame = cap.read()
+    # --------start...--------#
+    global show
+    show = False
+    num = 1
+    names = ['test']
+    ress = [0]
+    while ok:
+        frame = cv2.flip(frame, 1)
+        objs, crop_faces = detect_images(dbface, frame)
+
+        for img in crop_faces:
+            name, res = Face_comparison(net, img, face_data, thres=0.9)
+            names.append(name)
+            ress.append(res)
+        # 识别成功，显示名字
+        if num % 30 == 0:
+            realname = Counter(names).most_common(1)[0][0]  # 统计30帧内出现次数最多的名字
+            if realname == 'test':
+                res = 'UnRecognized'
+            else:
+                res = max(ress)
+            print(num, Counter(names), realname)
+            names = ['UnRecognized']  # 置为初值，避免报错
+            ress = [0]
+            show = True
+        if not show:
+            for obj in objs:  # 画检测框
+                common.drawbbox0(frame, obj)  # 画检测框
+        else:
+            for obj in objs:
+                common.drawbbox(frame, obj, realname, res)
+
+        num += 1
+        cv2.imshow("demo Face", frame)
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord('q'):
+            break
+        ok, frame = cap.read()
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    while 1:
+        x = input('1 or 2 :' + '\n')
+        if x == '1':
+            Face_Entry_Pro()
+        elif x == '2':
+            camera_demo0()
+        else:
+            print('666')
---
+++ Face/utils/db_pro.py
@@ -0,0 +1,222 @@
+import os
+
+from torch import nn
+import numpy as np
+import torch
+import torch.nn.functional as F
+import cv2
+from PIL import Image
+from utils import common
+from utils.common import new_crops
+from datas.get_data import transform
+from utils.DBFace import DBFace
+
+HAS_CUDA = torch.cuda.is_available()
+
+
+def nms(objs, iou=0.5):
+    if objs is None or len(objs) <= 1:
+        return objs
+
+    objs = sorted(objs, key=lambda obj: obj.score, reverse=True)
+    keep = []
+    flags = [0] * len(objs)
+    for index, obj in enumerate(objs):
+        if flags[index] != 0:
+            continue
+
+        keep.append(obj)
+        for j in range(index + 1, len(objs)):
+            if flags[j] == 0 and obj.iou(objs[j]) > iou:
+                flags[j] = 1
+    return keep
+
+
+def detect(model, image, threshold=0.4, nms_iou=0.5):
+    mean = [0.408, 0.447, 0.47]
+    std = [0.289, 0.274, 0.278]
+
+    image = common.pad(image)
+    image = ((image / 255.0 - mean) / std).astype(np.float32)
+    image = image.transpose(2, 0, 1)
+
+    torch_image = torch.from_numpy(image)[None]
+    if HAS_CUDA:
+        torch_image = torch_image.cuda()
+
+    hm, box, landmark = model(torch_image)
+    hm_pool = F.max_pool2d(hm, 3, 1, 1)
+    scores, indices = ((hm == hm_pool).float() * hm).view(1, -1).cpu().topk(1000)
+    hm_height, hm_width = hm.shape[2:]
+
+    scores = scores.squeeze()
+    indices = indices.squeeze()
+    ys = list((indices / hm_width).int().data.numpy())
+    xs = list((indices % hm_width).int().data.numpy())
+    scores = list(scores.data.numpy())
+    box = box.cpu().squeeze().data.numpy()
+    landmark = landmark.cpu().squeeze().data.numpy()
+
+    stride = 4
+    objs = []
+    for cx, cy, score in zip(xs, ys, scores):
+        if score < threshold:
+            break
+
+        x, y, r, b = box[:, cy, cx]
+        xyrb = (np.array([cx, cy, cx, cy]) + [-x, -y, r, b]) * stride
+        x5y5 = landmark[:, cy, cx]
+        x5y5 = (common.exp(x5y5 * 4) + ([cx] * 5 + [cy] * 5)) * stride
+        box_landmark = list(zip(x5y5[:5], x5y5[5:]))
+        objs.append(common.BBox(0, xyrb=xyrb, score=score, landmark=box_landmark))
+    return nms(objs, iou=nms_iou)
+
+
+# def detect_image(model, file):
+#     image = common.imread(file)
+#     objs = detect(model, image)
+#     coor = []
+#     left = []
+#     right = []
+#     for obj in objs:
+#         common.drawbbox(image, obj)
+#         # 取出框和眼睛坐标
+#         coor.append(common.intv(obj.box))
+#         left.append(common.intv(obj.landmark[0]))
+#         right.append(common.intv(obj.landmark[1]))
+#
+#     # 创建保存总目录
+#     if os.path.exists("../result/"):
+#         common.delete_folder_contents("../result/")
+#     if not os.path.exists("../result/"):
+#         os.makedirs("../result/")
+#
+#     dect_img_path = "../result/" + common.file_name_no_suffix(file) + "_dect.tiff"  # 人脸检测图像路径，用于qt显示
+#     common.imwrite(dect_img_path, image)  # 开始保存
+#     crop_faces, crop_save_path = new_crops(file, coor, left, right)  # 进行裁切人脸，    crop_save_path用于qt显示
+#
+#     return dect_img_path, crop_faces, crop_save_path,
+#     # 将裁切出来的人脸拿去提取特征，crop_faces[0 1 2] 里面包含检测到的脸，可以用cv2显示和保存
+
+
+# def image_demo(dbface, path):
+#     "QT中使用"
+#     path0, crop_faces, crop_save_path = detect_image(dbface, path)
+#     return path0, crop_faces, crop_save_path
+
+def detect_image(model, frame):
+    objs = detect(model, frame)
+    coor = []
+    left = []
+    right = []
+    for obj in objs:
+        # common_test.drawbbox(frame, obj)#
+        # 取出框和眼睛坐标
+        coor.append(common.intv(obj.box))
+        left.append(common.intv(obj.landmark[0]))
+        right.append(common.intv(obj.landmark[1]))
+
+    crop_faces = new_crops(frame, coor, left, right)
+    return objs, crop_faces
+
+
+def image_demos(path):
+    dbface = DBFace()
+    dbface.eval()
+    if HAS_CUDA:
+        dbface.cuda()
+    dbface.load('MyQT/QT_resources/dbface.pth')
+    frame = cv2.imread(path)
+    _, crop_faces = detect_image(dbface, frame)
+    return crop_faces
+
+
+def detect_images(model, frame):
+    objs = detect(model, frame)
+    coor = []
+    left = []
+    right = []
+    for obj in objs:
+        # common_test.drawbbox(frame, obj)  #
+        # 取出框和眼睛坐标
+        coor.append(common.intv(obj.box))
+        left.append(common.intv(obj.landmark[0]))
+        right.append(common.intv(obj.landmark[1]))
+
+    crop_faces = new_crops(frame, coor, left, right)  # 进行裁切人脸，    crop_save_path用于qt显示
+
+    return objs, crop_faces
+
+
+def compare(face1, face2):
+    "检测的人脸特征和库中特征比对，计算余弦相似度"
+    # "cos"  bug1   face1 [1,1000]  face2   [1000]
+    face1_norm = F.normalize(face1)
+    face2_norm = F.normalize(face2)
+    res = torch.matmul(face1_norm, face2_norm.t())
+    return res
+
+
+def compare1(face1, face2_list):
+    "传入检测到的人脸，和数据库中存在的某个人的人脸特征列表对比"
+    face1_norm = F.normalize(face1)
+    face2_list_norm = [F.normalize(face2) for face2 in face2_list]
+    res_list = []
+    for face2_norm in face2_list_norm:
+        res = torch.matmul(face1_norm, face2_norm.to('cpu').t())
+        res_list.append(res.item())
+    return res_list
+
+
+def Face_comparison(models, img, face_data, thres):
+    """调用摄像头识别"""
+    rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    i = Image.fromarray(rgb_img)  # 转为pil类型
+    with torch.no_grad():
+        p1_feature = models.get(transform(i).unsqueeze(0).cuda())
+
+    for key, value in face_data.items():
+        res = compare1(p1_feature.to('cpu'), value)
+        if sum(res) / len(res) > thres:  # 取平均
+            return key, torch.tensor(sum(res) / len(res))
+        else:
+            continue
+    return 'UnRecognized', 0
+
+
+class Arcsoftmax(nn.Module):
+    def __init__(self, feature_num, cls_num):
+        super().__init__()
+        self.w = nn.Parameter(torch.randn((feature_num, cls_num)))
+        self.func = nn.Softmax()
+
+    def forward(self, x, s=1, m=0.2):
+        x_norm = F.normalize(x, dim=1)
+        w_norm = F.normalize(self.w, dim=0)
+
+        cosa = torch.matmul(x_norm, w_norm) / 10
+        a = torch.acos(cosa)
+
+        arcsoftmax = torch.exp(
+            s * torch.cos(a + m) * 10) / (torch.sum(torch.exp(s * cosa * 10), dim=1, keepdim=True) - torch.exp(
+            s * cosa * 10) + torch.exp(s * torch.cos(a + m) * 10))
+
+        return torch.log(arcsoftmax)
+
+
+def add_filedoer(name, facelist, net, device, path):
+    # 是否存在本地库
+    if os.path.exists(path):
+        face_fea = torch.load(path)
+    else:
+        face_fea = {}
+    for i in facelist:
+        rgb_img = cv2.cvtColor(i, cv2.COLOR_BGR2RGB)
+        i = Image.fromarray(rgb_img)  # 转为pil类型
+        with torch.no_grad():
+            p1_feature = net.get(transform(i).unsqueeze(0).to(device))
+        if name not in face_fea:
+            face_fea[name] = [p1_feature]
+        else:
+            face_fea[name].append(p1_feature)
+    torch.save(face_fea, path)
